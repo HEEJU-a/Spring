82649863a28e37a3252578b9fd2b9553793db4d0
HEEJU-a <81272648+HEEJU-a@users.noreply.github.com>
2021-06-04 18:00:02 +0900

    Add files via upload

---
+++ spring1/src/main/java/com/koreait/spring/board/BoardController(2).java
@@ -0,0 +1,45 @@
+package com.koreait.spring.board;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @Autowired
+    private BoardService service;
+
+    @RequestMapping("/list")
+    public String list(Model model){
+        List<BoardDomain> list = service.selBoardList();
+        model.addAttribute("list", list);
+        return "board/list";
+    }
+
+    @RequestMapping("/detail")
+    public String detail(BoardDTO param, Model model){
+        System.out.println("iboard : " + param.getIboard());
+        BoardDomain data = service.selBoard(param);
+        model.addAttribute("data", data);
+        return"board/detail";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/cmtInsSel", method = RequestMethod.POST)
+    public Map<String, Integer> cmtInsSel(@RequestBody BoardCmtEntity param){
+        System.out.println("param = " + param);
+        Map<String, Integer> data = new HashMap<>();
+        data.put("result", 1);
+        return data;
+    }
+}
