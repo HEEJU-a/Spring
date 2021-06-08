47932c94f55ba4d8f486b1451ff3e1bebb35a363
HEEJU-a <81272648+HEEJU-a@users.noreply.github.com>
2021-06-08 16:52:17 +0900

    Add files via upload

---
+++ spring1/src/main/java/com/koreait/spring/user/UserController(2).java
@@ -0,0 +1,59 @@
+package com.koreait.spring.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(value="/login", method= RequestMethod.GET)
+    public String login(@RequestParam(value="err",defaultValue = "0") int err, Model model){
+        System.out.println("err: " + err);
+        switch (err){
+            case 1: //아이디 없음
+                model.addAttribute("errMsg","아이디를 확인해주세요.");
+                break;
+            case 2: //비밀번호 틀림
+                model.addAttribute("errMsg", "비밀번호를 확인해주세요.");
+                break;
+        }
+        return "user/login";
+    }
+
+    @RequestMapping(value="/login", method= RequestMethod.POST)
+    public String login(UserEntity param){
+
+        return "redirect:" + service.login(param);
+    }
+
+    @RequestMapping("/join")
+    public String join(){ return "user/join"; }
+
+    @RequestMapping(value="/join",method=RequestMethod.POST)
+    public String join(UserEntity param) {
+        System.out.println("uid" + param);
+        service.join(param);
+        return "redirect:/user/login";
+    }
+
+    @RequestMapping("/profile")
+    public String profile(){
+        return "user/profile";
+    }
+
+    //@RequestMapping(value="/profile", method = RequestMethod.POST)
+    @PostMapping("/profile")
+    public String profile(MultipartFile profileImg){
+        return "redirect:" + service.uploadProfile(profileImg);
+    }
+}
