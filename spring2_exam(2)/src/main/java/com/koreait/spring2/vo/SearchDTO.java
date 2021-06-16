a28921162637c1e9ea18fcea283f77f5ab9997b8
HEEJU-a <81272648+HEEJU-a@users.noreply.github.com>
2021-06-16 16:00:47 +0900

    Add files via upload

---
+++ spring2_exam(2)/src/main/java/com/koreait/spring2/vo/SearchDTO.java
@@ -0,0 +1,12 @@
+package com.koreait.spring2.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchDTO {
+    private int deal_year;
+    private int deal_month;
+    private String external_cd;
+}
